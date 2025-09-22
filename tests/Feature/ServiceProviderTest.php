1aef2b5323b3d6971ee8194778d926822115342c
el-schneider <mail@jonaslist.de>
2025-09-22 21:35:46 +0200

    test: add comprehensive test coverage for event handling and service provider

--- tests/Feature/ServiceProviderTest.php
+++ tests/Feature/ServiceProviderTest.php
@@ -1,7 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 it('can instantiate the service provider', function () {
-    $serviceProvider = app()->getProvider(\ElSchneider\StatamicAutoAltText\ServiceProvider::class);
+    $serviceProvider = app()->getProvider(ElSchneider\StatamicAutoAltText\ServiceProvider::class);
 
     expect($serviceProvider)->not->toBeNull();
 });
